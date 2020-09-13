34391d9c414ac444e619294d7fef3b8af97bf762
liang-faan <liang.faan@gmail.com>
2020-09-13 12:15:06 +0800

    customize the table page

--- Webconsole/src/containers/TheFooter.js
+++ Webconsole/src/containers/TheFooter.js
@@ -5,12 +5,12 @@ const TheFooter = () => {
   return (
     <CFooter fixed={false}>
       <div>
-        <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">CoreUI</a>
-        <span className="ml-1">&copy; 2020 creativeLabs.</span>
+        <a href="https://github.com/liang-faan" target="_blank" rel="noopener noreferrer">Github</a>
+        <span className="ml-1">&copy; liang-faan.</span>
       </div>
       <div className="mfs-auto">
         <span className="mr-1">Powered by</span>
-        <a href="https://coreui.io/react" target="_blank" rel="noopener noreferrer">CoreUI for React</a>
+        <a href="https://github.com/liang-faan" target="_blank" rel="noopener noreferrer">Liang Faan</a>
       </div>
     </CFooter>
   )
--- Webconsole/src/routes.js
+++ Webconsole/src/routes.js
@@ -38,6 +38,11 @@ const Widgets = React.lazy(() => import('./views/widgets/Widgets'));
 const Users = React.lazy(() => import('./views/users/Users'));
 const User = React.lazy(() => import('./views/users/User'));
 
+/**
+ * Custom pages
+ */
+const Dagtable = React.lazy(() => import('./views/airflow/Dagtable'));
+
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', component: Dashboard },
@@ -78,7 +83,11 @@ const routes = [
   { path: '/notifications/toaster', name: 'Toaster', component: Toaster },
   { path: '/widgets', name: 'Widgets', component: Widgets },
   { path: '/users', exact: true,  name: 'Users', component: Users },
-  { path: '/users/:id', exact: true, name: 'User Details', component: User }
+  { path: '/users/:id', exact: true, name: 'User Details', component: User },
+  /**
+   * Custom paths
+   */
+  { path: '/airflow/Dagtable', exact: true, name: 'Dagtable', component: Dagtable }
 ];
 
 export default routes;
