1d9f7538c6c542dc542301fae3fe9e8e14157387
liang-faan <liang.faan@gmail.com>
2020-11-22 12:35:47 +0800

    add swagger generate codes

---
+++ nodejs-server-server-generated/service/SearchService.js
@@ -0,0 +1,27 @@
+'use strict';
+
+
+/**
+ * searching books from elastic
+ *
+ * q String searching content
+ * returns List
+ **/
+exports.elasticSearch = function(q) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = [ {
+  "code" : 0,
+  "message" : "message"
+}, {
+  "code" : 0,
+  "message" : "message"
+} ];
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
